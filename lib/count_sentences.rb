336de925dc6dd8eb26c0d6e2ebc71027e90ef499
kyle molidor <k_molidor@yahoo.com>
2020-03-04 18:52:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,17 +7,26 @@ class String
       return true
     else
       return false
+    end
   end
 
   def question?
-    
+    if self.end_with? ("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with? ("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    new array = self.split
   end
 end
